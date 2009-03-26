49cc710a7c7e6413c2d2dab1473345bb4db49b85
Michel Albert <michel@albert.lu>
2009-03-26 18:20:40 +0100

    Renamed the files for consistency and to better reflect the project name

--- bin/cross_platform/tktttag.py
+++ bin/cross_platform/tktttag.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from Tkinter import Tk
 from tkSimpleDialog import askstring
 import sys
--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ setup(name='tagswarm',
    author_email='exhuma@foobar.lu',
    url='http://exhuma.github.com/tagswarm/',
    packages=['tagswarm'],
-   scripts=['bin/cross_platform/tstag',
-            'bin/cross_platform/tsuntag',
-            'bin/cross_platform/tsdumbdialog.py',
-            'bin/cross_platform/tsfastsearch']
+   scripts=['bin/cross_platform/tktttag.py',
+            'bin/cross_platform/tkuntag.py',
+            'bin/cross_platform/tkttfind.py',
+            'bin/cross_platform/tttag.py',
+            'bin/cross_platform/ttquery.py']
    )
 
