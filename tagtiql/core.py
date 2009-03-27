5a96a075577fd2b64c0b305daa186c7705f539a0
Michel Albert <michel@albert.lu>
2009-03-27 21:55:33 +0100

    It might be interesting to load the tags into memory on first use. This should work well until the machine runs out of memory ;)
    This commit provides a tiny working skeleton.
    
    Yet again a python 10-liner that's about to grow into a size that it
    requires some careful thought before proceeding. Otherwise this might
    evolve into a big mess!

--- tagtiql/core.py
+++ tagtiql/core.py
@@ -1,6 +1,6 @@
-from sqlite3 import IntegrityError, OperationalError, connect
+from sqlite3 import IntegrityError, OperationalError, connect, Connection
 from os.path import abspath, dirname, join, exists, basename, isdir
-from os import walk
+from os import walk, getcwd
 import sys
 
 DB_VERSION=2
@@ -8,13 +8,19 @@ IGNORED_FOLDERS = ['CVS', '.svn', '.git']
 OP_ADD_TAG    = 1
 OP_REMOVE_TAG = 2
 SEARCH_CONJUNCTIVE = 1
+CACHEDB = connect(":memory:")
+CACHE_INITIALISED = False
 
 def init_db(dbname):
-    try:
-       connection = connect( dbname )
-    except OperationalError, ex:
-       sys.stderr.write( "Unable to connect to the database %r. Errormessage was: %s\n" % ( dbname, str(ex) ) )
-       return
+
+    if not isinstance( dbname, Connection ):
+       try:
+          connection = connect( dbname )
+       except OperationalError, ex:
+          sys.stderr.write( "Unable to connect to the database %r. Errormessage was: %s\n" % ( dbname, str(ex) ) )
+          return
+    else:
+       connection = dbname
 
     c = connection.cursor()
     c.execute('''CREATE TABLE file_tags(
@@ -163,6 +169,64 @@ def collect_tags(path):
 
     return tags
 
+def aggregate_tagstores():
+   """
+   Construct or update an accumulated index of all known files and tags.
+   """
+   global CACHE_INITIALISED
+   swarm_files = []
+   for root, dirs, files in walk( getcwd() ):
+      for file in files:
+         if file == "swarmtags.sqlite3":
+            swarm_files.append( join( root, file ) )
+
+   if not CACHE_INITIALISED:
+      init_db(CACHEDB)
+      CACHE_INITIALISED = True
+
+   c = CACHEDB.cursor()
+
+   for tag_file in swarm_files:
+      tmpconn = connect( tag_file )
+      c2 = tmpconn.cursor()
+      c2.execute("SELECT * FROM file_tags")
+      tag_file_dir = dirname(tag_file)
+      for row in c2:
+         try:
+            c.execute( "INSERT INTO file_tags (file_name, tag) VALUES (?, ?)", [join(tag_file_dir, row[0]), row[1]] )
+         except IntegrityError, ex:
+            # duplicate entry
+            pass
+      c2.close()
+      tmpconn.close()
+
+   CACHEDB.commit()
+   c.close()
+
+def get_child_tags(parent_tags=None):
+   """
+   Retrieve a list of tags.
+   If parent_tags contains elements, only return tags which are attached to
+   files also having these parent_tags
+   """
+   global CACHE_INITIALISED
+
+   if not CACHE_INITIALISED:
+      init_db(CACHEDB)
+      CACHE_INITIALISED = True
+   c = CACHEDB.cursor()
+   if parent_tags:
+      subselects = [ "SELECT file_name FROM file_tags WHERE tag=?" for x in parent_tags ]
+      subq = " INTERSECT ".join( subselects )
+      q = "SELECT tag, COUNT(*) FROM file_tags WHERE file_name IN ("+subq+") GROUP BY tag ORDER BY COUNT(*) DESC"
+      c.execute( q, parent_tags )
+   else:
+      q = "SELECT tag, COUNT(*) FROM file_tags GROUP BY tag ORDER BY COUNT(*) DESC"
+      c.execute( q )
+
+   return list(c)
+
+   c.close()
 
 if __name__ == "__main__":
     import sys
